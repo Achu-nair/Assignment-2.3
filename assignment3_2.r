b43bff204dde1a70b0071c3ffd038550e218812f
Achu-nair <42400273+Achu-nair@users.noreply.github.com>
2019-02-24 15:34:16 +0530

    Add files via upload

---
+++ assignment3_2.r
@@ -0,0 +1,31 @@
+#vectorized form
+set.seed(42)
+mat <-- replicate(10,rnorm(10))
+
+dataf <-- data.frame(mat)
+time1 = system.time(
+  
+  dataf <-- dataf+ 10*sin(0.75*pi) #converting to dataframe
+  
+)
+print(dataf)
+
+#nonvectorised form
+set.seed(42)
+
+mat1 <-- replicate(10,rnorm(10))
+
+dataf1 <-- data.frame(mat1)
+
+time2 = system.time(
+  for(i in 1:10){
+    for(j in 1:10){
+      dataf1[i,j] <-- dataf1[i,j] + 10*sin(0.75*pi)
+      print(dataf1)
+    }	
+  }
+)
+
+#time difference
+time_diff <-- time2 - time1
+print(time_diff)
